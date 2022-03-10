c569147a08f58803b12f15d4a5d8852119c237b3
Jet Hu <774372605@qq.com>
2022-03-10 13:53:16 +0800

    fixed weird line

--- pages/video.js
+++ pages/video.js
@@ -1,15 +1,16 @@
 import Layout from "../components/layout";
+import styles from "../styles/Video.module.scss";
 
 export default function Video() {
   return (
     <Layout>
       <div className={`flex justify-center flex-col min-h-screen text-center`}>
         <div className={`relative overflow-hidden`}>
-          <video autoPlay muted loop playsInline className={`absolute w-11/12 h-5/6 top-2 left-5 object-cover object-center`} >
+          <video autoPlay muted loop playsInline className={`absolute w-full h-full top-0 left-0 object-cover object-center border border-white`} >
             <source src="https://uploads-ssl.webflow.com/61029a9e5385f704c8f71cbb/6114e997b7b7733955da822a_WhaleIVideo-transcode.mp4" type="video/mp4"></source>
           </video>
-          <h2 className="mix-blend-screen font-extrabold text-5xl sm:text-8xl bg-white -mx-px">Hello World</h2>
-          <h2 className="mix-blend-screen font-extrabold text-5xl sm:text-8xl bg-white -mx-px -mt-px">您好 世界</h2>
+          <h2 className="mix-blend-screen font-extrabold text-5xl sm:text-8xl bg-white">Hello World</h2>
+          <h2 className="mix-blend-screen font-extrabold text-5xl sm:text-8xl bg-white -mt-px">您好 世界</h2>
         </div>
       </div>
     </Layout>
