e9292b624f9d2a0096245859d6ac4878c5b6476e
Jet Hu <774372605@qq.com>
2022-03-14 16:30:41 +0800

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
+import * as ga from '../lib/ga'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return <Component {...pageProps} />
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Footer from '../components/Footer'
 import Heading from '../components/Heading'
 import VideoText from '../components/VideoText'
@@ -10,6 +11,19 @@ export default function Home() {
         <meta name="description" content="Know It is one of Jet Hu's web projects, it is a place to let people know who Jet Hu is and what he is doing">
         </meta>
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.TRACKING_ID}`}
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.TRACKING_ID});
+        `}
+      </Script>
       <Heading></Heading>
       <VideoText></VideoText>
       <Footer></Footer>
