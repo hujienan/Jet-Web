3457366ea3491f849bd913950dc293a42b1ac4f2
Jet Hu <774372605@qq.com>
2022-12-06 11:04:39 +0800

    update ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,16 @@ import '../styles/globals.css'
 import * as ga from '../lib/ga'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import * as ReactGA from 'react-ga'
+
+ReactGA.initialize('UA-158830260-1')
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+    // Track the page view with react-ga when the component mounts
+    ReactGA.pageview(window.location.pathname)
+  }, [])
   return <Component {...pageProps} />
 }
 
